7e86663ea8160d390b05ce660090e7080104aa71
Augusto Cesar Becker <augustobecker02@gmail.com>
2026-09-14 21:20:19 -0300

    Corrected the pull requests appointments:
    1) Removed old MC drivers;
    2) Included var_x to the calculation of b;
    3 and 4) Reverted the commit that implemented dask parallel computing;
    5) That was intended;
    6) Changed to 'MSD (dB)';
    7) Cleaned the experimental filter library;
    8) Cleaned the notebook.

--- filters.py
+++ filters.py
@@ -220,17 +220,12 @@ def GMVC_algorithm(N, x, d, h0, params):
             h = h + g*xtemp
     
     return filter_output(y=y, e=e, h=h_hist, v=np.zeros((N,L)))
-    #return h_hist, {'y': y, 'e': e}
 
 def sKF_algorithm(N, x, d, h0, parameters):
   h = h0
   epsilon = parameters.epsilon
   var_eta = parameters.var_eta
   v_tilde_0 = parameters.v_tilde_0
-
-  # normalize x
-  # regularization = 1e-3
-  # x_reg = np.sign(x) * (np.abs(x) + regularization)
 
   L = len(h)
   y = np.zeros((N,))
@@ -432,7 +427,7 @@ def sKF_L_exact_algorithm(N, x, d, h0, parameters):
     return filter_output(y=y, e=e, h=h_hist, v=v_hist)
 
 @njit(cache=True)
-def _generate_normal_input_signal(N:int, AR: NDArray[np.float64], warm_up: bool = True):
+def _generate_normal_input_signal(N:int, AR: NDArray[np.float64], warm_up: bool = True, var_x: float = 1.0):
     # Determine the input signal x through a AR process
     settling_time = AR_settling_time(AR, error = 0.001)*warm_up
     x = np.random.randn(N + settling_time)
@@ -440,7 +435,7 @@ def _generate_normal_input_signal(N:int, AR: NDArray[np.float64], warm_up: bool 
     # Generate the correlated signal
     AR = AR/AR[0]
     aux_Rxx = autocorr_matrix_calc(AR, 1, M = len(AR) - 1)
-    b = np.sqrt(1/aux_Rxx[0,0])
+    b = np.sqrt(var_x/aux_Rxx[0,0])
     x = filter(AR, np.array([b]), x)[settling_time:]
     
     return x
@@ -454,7 +449,7 @@ def std_gaussian_behavior(N: int, params: std_env_parameters, warm_up: bool = Tr
     v = np.sqrt(params.var_v)*np.random.randn(N)
 
     # Determine the input signal x through a AR process
-    x = _generate_normal_input_signal(N + L, params.AR, warm_up = warm_up)
+    x = _generate_normal_input_signal(N + L, params.AR, warm_up = warm_up, var_x = params.var_x)
 
     # Determine the desired signal
     d = v + np.convolve(params.ho, x, mode = 'full')[L:N+L]
@@ -464,7 +459,6 @@ def std_gaussian_behavior(N: int, params: std_env_parameters, warm_up: bool = Tr
 
 @njit(cache=True)
 def laplace_noise_behavior(N: int, params: laplace_env_parameters, warm_up: bool = True):
-#(N, ho, var_x, scale_v, AR, settling_time = 0):
     # AR process order and filter length
     L = len(params.ho)
 
@@ -472,37 +466,13 @@ def laplace_noise_behavior(N: int, params: laplace_env_parameters, warm_up: bool
     v = np.random.laplace(loc=0.0, scale=params.scale_v, size=(N,))
     
     # Determine the input signal x through a AR process
-    x = _generate_normal_input_signal(N + L, params.AR, warm_up = warm_up)
+    x = _generate_normal_input_signal(N + L, params.AR, warm_up = warm_up, var_x = params.var_x)
 
     # Determine the desired signal
     d = v + np.convolve(params.ho, x, mode = 'full')[L:N+L]
     x = x[L:N+L]
 
     return params.ho, {'x': x, 'v': v, 'd': d}
-
-@njit(cache=True)
-def std_behavior(N, ho, var_x, var_v, AR, settling_time = 0):
-  # AR process order and filter length
-  P = len(AR)
-  L = len(ho)
-
-  # Determine the noise signal
-  v = np.sqrt(var_v)*np.random.randn(N)
-
-  # Determine the input signal x through a AR process
-  x = np.random.randn(N + settling_time + L)
-
-  # Generate the correlated signal
-  AR = AR/AR[0]
-  aux_Rxx = autocorr_matrix_calc(AR, 1, M = len(AR) - 1)
-  b = np.sqrt(var_x/aux_Rxx[0,0])
-  x = filter(AR, np.array([b]), x)[settling_time:]
-
-  # Determine the desired signal
-  d = v + np.convolve(ho, x, mode = 'full')[L:N+L]
-  x = x[L:N+L]
-
-  return {'x': x, 'v': v, 'd': d}
 
 @njit(cache=True)
 def _compute_MSD(h_hist, ho):
@@ -568,82 +538,6 @@ def MC_Simulations(N,
         measures[label]['MSD'] /= NR
   
     return measures
-
-def MC_Simulations_Modular(N, NR, ho, var_x, var_v, h0, Algorithms, Parameters, AR, PBar = None):
-  L = len(h0)
-  N_Algorithms = len(Algorithms)
-
-  tau = AR_settling_time(AR)
-  measure_init = lambda taps, N_iter: {'h': np.zeros((N_iter, taps)),
-                                       'J': np.zeros(N_iter),
-                                       'Jex': np.zeros(N_iter)}
-
-  measures = {Parameters[k]["label"]: measure_init(L, N) for k in range(N_Algorithms)}
-
-  for k in range(NR):
-    signals = std_behavior(N, ho, var_x, var_v, AR, tau)
-    x = signals['x']
-    d = signals['d']
-
-    for c in range(N_Algorithms):
-      label = Parameters[c]["label"]
-      algorithm_signals = Algorithms[c](N, x, d, h0, Parameters[c])
-      measures[label]['h'] += algorithm_signals['h']
-      measures[label]['J'] += algorithm_signals['e']**2
-      measures[label]['Jex'] += (algorithm_signals['e']- signals['v'])**2
-
-    if not PBar is None:
-      PBar.update(1)
-    else:
-      print(f'Realization {k} out of {NR}')
-
-  for k in range(N_Algorithms):
-    label = Parameters[k]["label"]
-    measures[label]['h'] /= NR
-    measures[label]['J'] /= NR
-    measures[label]['Jex'] /= NR
-
-  return measures
-
-
-# COPY PASTE FOR RETURNING VARIANCE, FOR COMPARING
-def MC_Simulations_Modular_Variance(N, NR, ho, var_x, var_v, h0, Algorithms, Parameters, AR, PBar = None):
-  L = len(h0)
-  N_Algorithms = len(Algorithms)
-  tau = AR_settling_time(AR)
-  measure_init = lambda taps, N_iter: {'h': np.zeros((N_iter, taps)),
-                                       'J': np.zeros(N_iter),
-                                       'Jex': np.zeros(N_iter),
-                                       'var': np.zeros((N_iter, taps))}
-
-  measures = {Parameters[k]["label"]: measure_init(L, N) for k in range(N_Algorithms)}
-
-  for k in range(NR):
-    signals = std_behavior(N, ho, var_x, var_v, AR, tau)
-    x = signals['x']
-    d = signals['d']
-
-    for c in range(N_Algorithms):
-      label = Parameters[c]["label"]
-      algorithm_signals = Algorithms[c](N, x, d, h0, Parameters[c])
-      measures[label]['h'] += algorithm_signals['h']
-      measures[label]['J'] += algorithm_signals['e']**2
-      measures[label]['Jex'] += (algorithm_signals['e']- signals['v'])**2
-      measures[label]['var'] += algorithm_signals['v']
-
-    if not PBar is None:
-      PBar.update(1)
-    else:
-      print(f'Realization {k} out of {NR}')
-
-  for k in range(N_Algorithms):
-    label = Parameters[k]["label"]
-    measures[label]['h'] /= NR
-    measures[label]['J'] /= NR
-    measures[label]['Jex'] /= NR
-    measures[label]['var'] /= NR
-
-  return measures
 
 gaussian_params = np.dtype([("mean", "f8"),
                             ("variance", "f8")])
