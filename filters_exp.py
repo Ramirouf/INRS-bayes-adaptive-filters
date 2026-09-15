8a910a16da9a56dd6588d179615c9bedd460d5e8
AugustoCBecker <94455701+AugustoCBecker@users.noreply.github.com>
2026-09-15 10:34:13 -0300

    Augusto (#1)
    * Implemented experimental MC Carlo function with modular environment. Just select/create an environment and use in the MC simulation.
    
    * Implemented GMVC algorithm and new NLMS parameter type.
    
    * Implemented experimental MC MSD calculation (pending to change output type of remaining algorithms for numba compatibility)
    
    * Implemented compatibility of every filter with the new MC_Simulation function.
    
    * Corrected the pull requests appointments:
    
    1) Removed old MC drivers;
    2) Included var_x to the calculation of b;
    3 and 4) Reverted the commit that implemented dask parallel computing;
    5) That was intended;
    6) Changed to 'MSD (dB)';
    7) Cleaned the experimental filter library;
    8) Cleaned the notebook.

---
+++ filters_exp.py
@@ -0,0 +1,7 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from numba import njit
+from numba_progress import ProgressBar
+from collections import namedtuple
+
+from numpy.typing import NDArray
