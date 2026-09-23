a17283f3c3d26955686dae726d480a3f511776e7
Augusto Cesar Becker <augustobecker02@gmail.com>
2026-09-23 14:32:33 -0300

    Transferred dask_MC_simulation to filters.exp.

--- filters.py
+++ filters.py
@@ -22,6 +22,8 @@ import numpy as np
 import scipy.signal
 import scipy
 import numba
+import dask
+from dask.diagnostics import ProgressBar as dask_PB
 from numba import njit, prange, objmode, types
 from typing import List, Optional, Any, AnyStr, TypedDict
 from collections.abc import Callable
@@ -133,7 +135,6 @@ def AR_settling_time(AR, error = 0.01):
 
   settling_time = np.log(error)/np.log(slowest_decay_ratio)
   return int(np.ceil(settling_time))
-
 
 @njit(cache=True, nogil=True)
 def shift(new_x_sample, x_window):
@@ -541,6 +542,69 @@ def MC_Simulations(N,
           measures[label]['var'] /= NR
           measures[label]['MSD'] /= NR
   
+    return measures
+
+def sum_measures(measures_a, measures_b):
+    summed_measures = {}
+    for label in measures_a:
+        summed_measures[label] = {}
+        for key in measures_a[label]:
+            summed_measures[label][key] = measures_a[label][key] + measures_b[label][key]
+    return summed_measures
+
+delayed_chunked_iterations = dask.delayed(MC_Simulations)
+delayed_sum = dask.delayed(sum_measures)
+
+def create_tasks_tree(tasks):
+    while len(tasks) > 1:
+        next_stage = []
+        for i in range(0, len(tasks), 2):
+            if i + 1 < len(tasks):
+                combined = delayed_sum(tasks[i], tasks[i+1])
+                next_stage.append(combined)
+            else:
+                next_stage.append(tasks[i])
+        tasks = next_stage
+    return tasks[0]
+
+def dask_MC_Simulations(N, 
+                        NR,
+                        environment_parameters,
+                        environment,
+                        Algorithms,
+                        Parameters,
+                        h0,
+                        num_workers = 1,
+                        num_chunks = None):
+    L = len(h0)
+    N_Algorithms = len(Algorithms)
+    if num_chunks is None:
+        num_chunks = num_workers
+
+    rest_of_realizations = NR % num_chunks
+    tasks = [delayed_chunked_iterations(
+        N, NR//num_chunks, environment_parameters, environment, Algorithms, Parameters, h0, "", external_avg = True
+    ) for _ in range(num_chunks)]
+    
+    if rest_of_realizations > 0:
+        tasks.append(delayed_chunked_iterations(
+            N, rest_of_realizations, environment_parameters, environment, Algorithms, Parameters, h0, "", external_avg = True
+        ))
+    
+    tasks_tree = create_tasks_tree(tasks)
+    tasks_tree.visualize()
+
+    with dask_PB():
+        measures = dask.compute(tasks_tree, num_workers=num_workers)[0]
+
+    for k in range(N_Algorithms):
+        label = Parameters[k].label
+        measures[label]['h'] /= NR
+        measures[label]['J'] /= NR
+        measures[label]['Jex'] /= NR
+        measures[label]['var'] /= NR
+        measures[label]['MSD'] /= NR
+
     return measures
 
 gaussian_params = np.dtype([("mean", "f8"),
